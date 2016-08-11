8db5e8968d7bbf2b40b053a354a78b5efd972f8b
micke <micke@DESKTOP-CAPUD0J>
2016-08-11 11:40:40 +0300

    iWindow class was added to GUI - control opened windows

---
+++ src/gui/iWindow.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This Class controls the opened windows and prevents more than one shown window
+ * Other windows will remain active but at hide state;
+ */
+package gui;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+/**
+ * @author mickey
+ */
+public class iWindow {
+    protected static boolean isInternalOpened = false;
+    protected static JInternalFrame currentWindow = null;
+    
+    
+    
+    
+    //================================= Setters and Getters ==================================
+    protected static void setWindowState(boolean state){
+        isInternalOpened = state;
+    }
+    
+    protected static boolean getWindowState(){
+        return isInternalOpened;
+    }
+    
+    protected static void setCurrentWindow(JInternalFrame frame){
+        currentWindow = frame;
+    }
+    
+    protected static JInternalFrame getCurrentWindow(){
+        return currentWindow;
+    }
+    
+    
+    //================================ Methods =========================================
+    
+    /**
+     * This method controls the opened internal windows by hiding and showing desired windows
+     * @param frame 
+     */
+    protected static void openWin(JInternalFrame frame){
+        if (!getWindowState()){
+            setCurrentWindow(frame);
+            frame.setVisible(true);
+            setWindowState(true);
+        }else{
+            currentWindow.setVisible(false);
+            setCurrentWindow(frame);
+            currentWindow.setVisible(true);
+            setWindowState(true);
+        }
+
+        
+        return;
+    }
+    
+    
+}
